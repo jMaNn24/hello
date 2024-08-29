cd9c669f9edaffb975bc52ecb996ddd8e5d059a6
jMaNn24 <jhoffman@huskers.unl.edu>
2024-08-29 09:50:51 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Jamison Hoffman
+ *         EMAIL
+ * Date: 2024/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jamison Hoffman\n");
+}
